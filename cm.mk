806120009eda00d8e2a06bd0044f11c1a251d17e
JackpotClavin <jonclavin@gmail.com>
2015-09-13 18:18:33 -0400

    Ensure the 64-bit configs are the first thing inherited.

--- cm.mk
+++ cm.mk
@@ -2,6 +2,9 @@
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 2560
 
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
