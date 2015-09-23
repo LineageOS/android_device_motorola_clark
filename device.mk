4c1d4fb96aac0f376f082fd1d94eb460f018a086
scott <scott@ubuntu>
2015-09-23 19:53:14 -0400

    fix path for nfc
    Signed-off-by: scott <scott@ubuntu>

--- device.mk
+++ device.mk
@@ -191,8 +191,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    device/moto/clark/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/moto/clark/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf
 
 # Audio effects
 PRODUCT_PACKAGES += \
