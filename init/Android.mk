2f9d02d19cac82b83d501000eaef66916685e0fa
Griffin Millender <griffinn.millender@gmail.com>
2015-11-03 18:25:45 -0600

    clark: build init as c++
    Change-Id: I815fec60d917a761aa9cd1865899b2ff675cacbe

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_clark.c
+LOCAL_SRC_FILES := init_clark.cpp
 LOCAL_MODULE := libinit_clark
 include $(BUILD_STATIC_LIBRARY)
