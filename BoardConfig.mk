4e25c0bef9ae72aa5db67054299fb664a85cf1fe
HashBang <hashbang173@gmail.com>
2015-12-13 14:24:46 -0500

    Camera changes
    Change-Id: Ie684956d7892d80269bc27102b1fe4752e026150

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,12 +146,6 @@ PROTOBUF_SUPPORTED := true
 #Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 
-TARGET_USES_AOSP := true
-# Force camera module to be compiled only in 32-bit mode on 64-bit systems
-# Once camera module can run in the native mode of the system (either
-# 32-bit or 64-bit), the following line should be deleted
-BOARD_QTI_CAMERA_32BIT_ONLY := true
-
 #Enable peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
