abea072ce83788f7c1dfcf3b9630a29b1eca7d81
HashBang <hashbang173@gmail.com>
2016-01-02 17:27:56 -0500

    clark: prebuilt audio
    Change-Id: I951eeaf9178b1258ec751c8674e4735e0c9ead0e

--- device.mk
+++ device.mk
@@ -216,6 +216,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/audio/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/audio/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:system/etc/audio_ext_spkr.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # CMActions
