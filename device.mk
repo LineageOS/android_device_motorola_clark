8306ac71119c166bcb437ce965e90bfa575aa1a1
Stephen Bird <sbird@cyngn.com>
2016-02-04 02:25:09 -0800

    Fix charging mode and remove some unused ramdisk items
    Change-Id: Ie5a75c5e377a922cffef7e529bb382e70f3e4ba9

--- device.mk
+++ device.mk
@@ -136,7 +136,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.mmi.boot.sh \
     init.mmi.usb.rc \
-    init.mmi.usb.sh \
     init.qcom.rc \
     init.qcom.power.rc \
     init.mmi.block_perm.sh \
