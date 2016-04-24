231a942ceb0c9675d2ea3abc8a101a712f786a40
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-04-24 00:15:59 -0500

    clark: remove duplicate package and non-existant boot jar
    Change-Id: I08e5f0f722c6113977b4dc7258dffeb86bca5e94

--- device.mk
+++ device.mk
@@ -156,7 +156,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libstagefrighthw \
     libc2dcolorconvert \
     libdivxdrmdecrypt \
     libOmxAacEnc \
@@ -166,9 +165,6 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
-
-PRODUCT_BOOT_JARS += \
     libstagefrighthw
 
 # Lights
