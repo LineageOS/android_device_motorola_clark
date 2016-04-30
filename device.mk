385fdf6c75af7f82257d367ae712722fd778cb5f
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-04-30 05:52:52 -0500

    wifi: switch to stock wcnss ini and remove usb ini
    * sync wcnss config to stock (MPH24.49-18)
    * usb ini doesn't seem to be used, remove it
    
    Change-Id: I3b2bbcd7850f8225c37957f20b33a287dd27d443

--- device.mk
+++ device.mk
@@ -208,8 +208,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.usb.ini:system/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.usb.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
