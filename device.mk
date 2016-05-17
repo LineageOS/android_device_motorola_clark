41521ae69a9d0733c7985cc44ea6206291837de0
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-16 20:05:45 -0500

    clark: fix audio effects
    * use stock audio_effects.conf
    * add audio_ext_spkr.conf (needed by libspeakerbundle.so)
    * fix path to aanc_tuning_mixer.txt
    * set call volume steps to 8 (per stock)
    
    Change-Id: I42041cfd59eb74261f59fbd666aea0b8fee01efa

--- device.mk
+++ device.mk
@@ -19,7 +19,9 @@ $(call inherit-product-if-exists, vendor/motorola/clark/clark-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/vendor/etc/aanc_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:system/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/media_codecs.xml:system/etc/media_codecs.xml \
