961497186b790053ac011ec8369226cc1365b362
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-06-16 08:53:35 -0500

    clark: move a few configs from vendor repo
    Change-Id: If68b8d5710c798cc3f115771c697718ae744e60e

--- device.mk
+++ device.mk
@@ -90,6 +90,7 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
 
 # Init
@@ -202,6 +203,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/configs/sensorhub-blacklist.txt:system/etc/firmware/sensorhub-blacklist.txt
 
 # Thermal
