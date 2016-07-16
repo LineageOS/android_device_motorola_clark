007e399a456a57cba380f07a2b3d32acc1e51ea1
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-07-16 05:49:11 -0700

    clark: Copy audio_effects in /vendor/etc
    Change-Id: Id794174a74066b8e5486213576620de08617b727

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ $(call inherit-product-if-exists, vendor/motorola/clark/clark-vendor.mk)
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:system/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
