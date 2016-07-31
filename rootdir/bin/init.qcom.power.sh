ee7b88162ff267281f7934bb94e2e04697273669
HashBang <hashbang173@gmail.com>
2016-07-31 17:17:07 -0400

    DO NOT MERGE: Do not load and configure core ctl
    Change-Id: I60c10722a2cae7d1b4035f00d00aae33edd4ca1c

--- rootdir/bin/init.qcom.power.sh
+++ rootdir/bin/init.qcom.power.sh
@@ -84,21 +84,6 @@ write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 79000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif 1
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
 
-# Enable and configure core_ctl (if available)
-if [ -e /system/lib/modules/msm_core_ctl.ko ]; then
-    insmod /system/lib/modules/msm_core_ctl.ko
-    if [ $? -eq 0 ]; then
-        restorecon -R /sys/devices/system/cpu # must restore after insmod
-        write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 2
-        write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 0
-        write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
-        write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
-        write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 100
-        write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
-        write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
-    fi
-fi
-
 # Available Freqs in stock kernel
 # Little: 384000 460800 600000 672000 787200 864000 960000 1248000 1440000
 # Big: 384000 480000 633600 768000 864000 960000 1248000 1344000 1440000 1536000 1632000 1689600 1824000
