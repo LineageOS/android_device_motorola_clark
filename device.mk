d212f3755005d023700da0508f0ee3e2136efe91
Hashbang173 <hashbang173@gmail.com>
2016-08-27 11:59:13 -0400

    [DO NOT MERGE] clark: temp fixes for N
    Change-Id: Ic198f718ba0babcdc1e0a425b3bc6aa26c2e289a

--- device.mk
+++ device.mk
@@ -62,7 +62,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/com.motorola.cameraone.xml:system/etc/permissions/com.motorola.cameraone.xml
 
 # CMActions
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     CMActions \
     libjni_CMActions
 
