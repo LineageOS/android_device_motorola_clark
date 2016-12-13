14ca83fc085f72a0c1cd163ea85027498aa7c4ff
Alberto97 <albertop2197@gmail.com>
2016-12-13 22:10:16 +0100

    clark: Add IMS shim
    Change-Id: Ic61e759337da61dcf3096e5bc1943b4eabfa7133

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -26,3 +26,15 @@ LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
+
+# IMS
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := MediaBuffer.c
+
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
