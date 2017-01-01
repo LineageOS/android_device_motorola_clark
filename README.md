858c5fe255756bc7c60780d169593d8085c4d6e6
Hashbang173 <hashbang173@gmail.com>
2017-01-01 17:30:44 -0500

    clark: rebrand to LineageOS
    Change-Id: If0f157970acbaac6edb7c3899fb58371703e7fe7

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Copyright 2016 - The CyanogenMod Project
 
+Copyright 2017 - The LineageOS Project
+
 Device configuration for Moto X Pure Edition/Style (2015)
 =====================================
 
--- lineage.mk
+++ lineage.mk
@@ -4,13 +4,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_clark
+PRODUCT_NAME := lineage_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
