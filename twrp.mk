017019587867eec0bbae9259b7452157f69b0070
Dan Pasanen <dan.pasanen@gmail.com>
2017-02-03 09:50:14 -0600

    clark: add twrp flags/fstab and crypto blobs
    Change-Id: Ibc7cd3f5fbcadbca4fe154c1a048293a342f444e

---
+++ twrp.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += device/motorola/clark/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+
+RECOVERY_VARIANT := twrp
+
+DEVICE_RESOLUTION := 1440x2560
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_L_CRYPTO :=true
+TW_NEW_ION_HEAP := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_QCOM_RTC_FIX := true
