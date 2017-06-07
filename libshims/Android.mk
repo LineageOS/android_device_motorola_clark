2052088f034fc248bb66676699f9488cd9f52f84
Alberto97 <albertop2197@gmail.com>
2017-06-07 09:53:30 +0200

    clark: provide ASensor_getHandle for libmdmcutback
    Change-Id: I6ce0f2446a6c03680d8216df3c58c54498d7c5bf

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -26,3 +26,11 @@ LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+# RIL
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sensor.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
