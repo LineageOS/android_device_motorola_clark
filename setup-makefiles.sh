c351c9e9add7fb227080d7d29ea1016ce4b0f8d6
rob e anderson <obsolete.ra@gmail.com>
2017-07-18 10:24:55 -0400

    Remove some old CM stuff that is not needed

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -27,18 +27,6 @@ INITIAL_COPYRIGHT_YEAR=2016
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
 # Copyright headers and guards
 write_headers
 
