21ff0e2a06986d90820668e84fd87662570520df
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-22 23:02:27 +0300

    clark: Replace vendor/cm with vendor/lineage
    Change-Id: I1bdc156a1b8f88f05af8e2b3f8ffaef7a578150f

--- lineage_clark.mk
+++ lineage_clark.mk
@@ -5,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
