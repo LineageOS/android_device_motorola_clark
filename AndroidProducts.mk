c07edf6f5de77dbf91866a6843b55883f25e1b3c
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    clark: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2015 The Android Open Source Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +17,7 @@
 
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lineage_clark.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_clark-userdebug \
+    lineage_clark-eng
