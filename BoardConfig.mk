05cb8db730ce6091fdec5169022b588f0b27fe1f
Michael W <baddaemon87@gmail.com>
2018-12-06 20:27:53 +0100

    clark: Adapt to new bootimage name
    * Commit edc1cebc2f7baedf6479874db2848b45e22f4205 introduced a new way
      to name bootimages
    * Ours is arm64, uncompressed and separated DT, so the name is just "Image"
    
    Change-Id: I7a7df99d327b516d2fe7a29a5b7907acea045c67

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=none androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 earlyprintk=msm_hsl_uart,0xf991e000 utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup boot_cpus=0-5
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE :=  4096
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -122,7 +123,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := clark_defconfig
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8992
-TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
