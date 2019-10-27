975718182ecace432e2089018803f5058017c159
bgcngm <bgcngm@gmail.com>
2019-10-27 18:43:19 +0000

    clark: Drop mediaextractor seccomp policy
    * These syscalls are now allowed by default.
    
    * [arco] Also clean mediacodec.policy
    
    Change-Id: I21592135dfba78886ea17d7c95bac17778d60b59

--- device.mk
+++ device.mk
@@ -273,8 +273,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
