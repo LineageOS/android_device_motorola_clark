e31e40310fbb57902e6cea9845f108505153019c
chrmhoffmann <chrmhoffmann@gmail.com>
2020-05-24 16:12:54 +0200

    clark: Compile gps libs on Q
    Change-Id: I4f82d061677f562c6a02d9cdcd3b1212674d1845

--- device.mk
+++ device.mk
@@ -127,7 +127,15 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service \
     gps.msm8992
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
