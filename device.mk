298a7a06cb59b200254b5a09484835fabde3bfda
chrmhoffmann <chrmhoffmann@gmail.com>
2020-05-24 17:53:33 +0200

    clark: Drop QC location stack entirely
    Change-Id: Ia94613ae57402df2f327a47c1d1a5ec9a497c1fc

--- device.mk
+++ device.mk
@@ -132,13 +132,7 @@ PRODUCT_PACKAGES += \
     libshims_get_process_name
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
 # Init
 PRODUCT_PACKAGES += \
