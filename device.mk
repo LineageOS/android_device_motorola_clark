f4aeceb998b7434343d263a87b68bb53ae79abd9
chrmhoffmann <chrmhoffmann@gmail.com>
2020-05-25 19:07:48 +0200

    clark: Build trust HIDL HAL interfaces
    Change-Id: Id830ba0b52293bb1aacf785330427d9c68507f37

--- device.mk
+++ device.mk
@@ -277,6 +277,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
