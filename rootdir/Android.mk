6fdc1355f2a2a7f620492353e2d93aacd42c2274
chrmhoffmann <chrmhoffmann@gmail.com>
2020-07-14 19:07:08 +0200

    clark: Remove bt cruft
    * hciattach is not called on rome
    * use BT_READ_ADDR_FROM_PROP and we can drop btnv blobs
    
    Change-Id: Iab744409e6ff619a86761cc21eedc018260db01a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -27,14 +27,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := init.qcom.bt.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
