a5bec7a9a1a3d2df24190f5730d810be3a181f12
chrmhoffmann <chrmhoffmann@gmail.com>
2020-08-01 09:24:56 +0200

    clark: power tuning from stock 25.201.1
    Change-Id: Ica4dd9804275cf82c0604cf586f5eb0b2f879fb5

--- rootdir/init.qcom.power.sh
+++ rootdir/init.qcom.power.sh
@@ -53,15 +53,14 @@ write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 95
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 39000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 90
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 787200
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:75 960000:80"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 39000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 79000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads 90
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
 
 # Make sure CPU 4 is only to configure big settings
@@ -74,14 +73,13 @@ restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 90
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 39000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 79000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads 85
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
 
 # Configure core_ctl
@@ -90,23 +88,10 @@ write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 2
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 100
-write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
+write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 2
 write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
-
-write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 0
-write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres 0
-write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 100
-write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 1
-
-# Available Freqs in stock kernel
-# Little: 384000 460800 600000 672000 787200 864000 960000 1248000 1440000
-# Big: 384000 480000 633600 768000 864000 960000 1248000 1344000 1440000 1536000 1632000 1689600 1824000
-write /sys/module/cpu_boost/parameters/boost_ms 20
-write /sys/module/cpu_boost/parameters/sync_threshold 960000
-write /sys/module/cpu_boost/parameters/input_boost_freq 0:787200
-write /sys/module/cpu_boost/parameters/input_boost_ms 40
 
 # b.L scheduler parameters
 write /proc/sys/kernel/sched_migration_fixup 1
