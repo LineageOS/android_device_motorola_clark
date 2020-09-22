a2455aaf86be796f5f3c879d4a03afe405fe68a1
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    clark: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -220,7 +220,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
+    android.hardware.power-service-qti \
     power.msm8992
 
 # Privapp Whitelist
